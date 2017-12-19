6930113c61f8b825d1c0885ad4680fb4086ae132
Rick G <richkws@gmail.com>
2017-12-19 23:56:55 +0000

    Assignment 19 - Clean up errors/whitespace and refactor remove_entry

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -13,14 +13,13 @@ RSpec.describe AddressBook do
       expect(book.entries).to be_an(Array)
     end
     
-    it "responds to entries" do
+    it "initializes entries as empty" do
       book = AddressBook.new
       expect(book.entries.size).to eq(0)
     end
   end
     
   describe "#add_entry" do
-    
     it "adds only one entry to the address book" do
       book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
@@ -36,8 +35,41 @@ RSpec.describe AddressBook do
       expect(new_entry.name).to eq('Ada Lovelace')
       expect(new_entry.phone_number).to eq('010.012.1815')
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
-      
     end
   end
   
+  
+  describe "#remove_entry" do
+    it "removes one entry from the Address Book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('John Doe', '123.456.7889', 'john.doe@gmail.com')
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      
+      expect(book.entries.size).to eq(1)
+    end
+      
+      
+    it "removes the correct entry from the book and doesn't remove others" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('John Doe', '123.456.7889', 'john.doe@gmail.com')
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+  
+      #check that the other entry is still there
+      expect(book.entries[0].name).to eq('John Doe')
+      expect(book.entries[0].phone_number).to eq('123.456.7889')
+      expect(book.entries[0].email).to eq('john.doe@gmail.com')
+      
+      #check the correct entry has been removed
+      found = false
+      book.entries.each do |entry|
+        if entry.name == 'Ada Lovelace'
+          found == true
+        end
+      end
+      
+      expect(found).to eq(false)
+    end
+  end
 end
--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -3,39 +3,33 @@ require_relative '../models/entry'
 
 RSpec.describe Entry do
   
-  describe "atttributes" do
-    
+  describe "attributes" do
     #define an entry method once, for use in several tests
     let(:entry) {Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')}
     
     it "responds to name" do
-      
       expect(entry).to respond_to(:name) # test whether we can call the method .name on that object
     end
     
-    it "reports it's name" do
+    it "reports its name" do
       expect(entry.name).to eq('Ada Lovelace')
     end
-    
     
     it "responds to phone number" do
        expect(entry).to respond_to(:phone_number)
     end
     
-    
-    it "reports it's phone number" do
+    it "reports its phone number" do
       expect(entry.phone_number).to eq('010.012.1815')
     end
-    
   
     it "responds to email" do
       expect(entry).to respond_to(:email)
     end
     
-    it "reports it's email" do
+    it "reports its email" do
       expect(entry.email).to eq('augusta.king@lovelace.com')
     end
-    
   end
   
   describe "#to_s" do   #testing an instance method
@@ -45,11 +39,4 @@ RSpec.describe Entry do
       expect(entry.to_s).to eq(expected_string)
     end
   end
-  
- 
- 
- 
- 
- 
-  
 end
