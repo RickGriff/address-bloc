287ba0819a12649beb4352e48090ed20cec05cee
Rick G <richkws@gmail.com>
2017-12-29 17:37:52 +0000

    Assignment 22 Searching: Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -59,15 +59,18 @@ class AddressBook
         lower = mid + 1
       end
     end
-
+    
     return nil
   end
   
   def iterative_search(name)
+    entries.each do |entry| 
+      if entry.name == name
+        return entry
+      end
+    end
+    
+    return nil
   end
-  
-  
 end
-
-
 
